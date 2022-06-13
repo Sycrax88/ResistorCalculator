c2a2eabe5a1961a32866b6c2e6f2022b8fbea3c5
Sebastian Ospina Garcia <sebastian47121@gmail.com>
2022-06-13 15:18:44 -0500

    Nuevo Branch Version 2.0 y se agrega el MainViewModel

---
+++ app/src/main/java/com/colosoft/resistorcalculator/ui/main/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.colosoft.resistorcalculator.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainViewModel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_view_model)
+    }
+}
