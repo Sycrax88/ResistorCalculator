539c64771deb4a17560924e6bc1e2118373d3cba
Sebastian Ospina Garcia <sebastian47121@gmail.com>
2022-06-13 16:23:10 -0500

    Se agregó el código al MainViewModel, y se modificó MainActivity para que funcione correctamente junto al MainViewModel

--- app/src/main/java/com/colosoft/resistorcalculator/ui/main/MainActivity.kt
+++ app/src/main/java/com/colosoft/resistorcalculator/ui/main/MainActivity.kt
@@ -1,97 +1,105 @@
 package com.colosoft.resistorcalculator.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.colosoft.resistorcalculator.R
-import android.widget.Toast
 import com.colosoft.resistorcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
 
-    private var first=0.0
-    private var second=0.0
-    private var third=1.0
-    private var tolerance=0.05
+
     private var total = 0.0
     private var tolerance_calculation = 0.0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = mainBinding.root
-
-
         setContentView(view)
 
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        mainViewModel.mutableTotalDone.observe(this) { mutableTotal ->
+            total = mutableTotal
+        }
+        mainViewModel.mutableTotalToleranceDone.observe(this) { mutableTotalTolerance ->
+            tolerance_calculation = mutableTotalTolerance
+        }
+
+        mainViewModel.initialize()
+
+
         mainBinding.toggleButtonGroup1.addOnButtonCheckedListener { toggleButtonGroup1, chekedId, isChecked ->
-            if (isChecked){
-                when (chekedId){
-                    R.id.black_button1 -> first=    0.0
-                    R.id.brown_button1 -> first=    10.0
-                    R.id.red_button1 -> first=      20.0
-                    R.id.orange_button1 -> first=   30.0
-                    R.id.yellow_button1 -> first=   40.0
-                    R.id.green_button1 -> first=    50.0
-                    R.id.blue_button1 -> first=     60.0
-                    R.id.purple_button1 -> first=   70.0
-                    R.id.gray_button1 -> first=     80.0
-                    R.id.white_button1 -> first=    90.0
+            if (isChecked) {
+                when (chekedId) {
+                    R.id.black_button1 -> mainViewModel.assignValue("black", 1)
+                    R.id.brown_button1 -> mainViewModel.assignValue("brown", 1)
+                    R.id.red_button1 -> mainViewModel.assignValue("red", 1)
+                    R.id.orange_button1 -> mainViewModel.assignValue("orange", 1)
+                    R.id.yellow_button1 -> mainViewModel.assignValue("yellow", 1)
+                    R.id.green_button1 -> mainViewModel.assignValue("green", 1)
+                    R.id.blue_button1 -> mainViewModel.assignValue("blue", 1)
+                    R.id.purple_button1 -> mainViewModel.assignValue("purple", 1)
+                    R.id.gray_button1 -> mainViewModel.assignValue("gray", 1)
+                    R.id.white_button1 -> mainViewModel.assignValue("white", 1)
                 }
             }
 
         }
 
         mainBinding.toggleButtonGroup2.addOnButtonCheckedListener { toggleButtonGroup2, chekedId, isChecked ->
-            if (isChecked){
-                when (chekedId){
-                    R.id.black_button2 -> second=   0.0
-                    R.id.brown_button2 -> second=   1.0
-                    R.id.red_button2 -> second=     2.0
-                    R.id.orange_button2 -> second=  3.0
-                    R.id.yellow_button2 -> second=  4.0
-                    R.id.green_button2 -> second=   5.0
-                    R.id.blue_button2 -> second=    6.0
-                    R.id.purple_button2 -> second=  7.0
-                    R.id.gray_button2 -> second=    8.0
-                    R.id.white_button2 -> second=   9.0
+            if (isChecked) {
+                when (chekedId) {
+                    R.id.black_button2 -> mainViewModel.assignValue("black", 2)
+                    R.id.brown_button2 -> mainViewModel.assignValue("brown", 2)
+                    R.id.red_button2 -> mainViewModel.assignValue("red", 2)
+                    R.id.orange_button2 -> mainViewModel.assignValue("orange", 2)
+                    R.id.yellow_button2 -> mainViewModel.assignValue("yellow", 2)
+                    R.id.green_button2 -> mainViewModel.assignValue("green", 2)
+                    R.id.blue_button2 -> mainViewModel.assignValue("blue", 2)
+                    R.id.purple_button2 -> mainViewModel.assignValue("purple", 2)
+                    R.id.gray_button2 -> mainViewModel.assignValue("gray", 2)
+                    R.id.white_button2 -> mainViewModel.assignValue("white", 2)
                 }
             }
         }
 
         mainBinding.toggleButtonGroup3.addOnButtonCheckedListener { toggleButtonGroup3, chekedId, isChecked ->
-            if (isChecked){
-                when (chekedId){
-                    R.id.black_button3 -> third=    1.0
-                    R.id.brown_button3 -> third=    10.0
-                    R.id.red_button3 -> third=      100.0
-                    R.id.orange_button3 -> third=   1000.0
-                    R.id.yellow_button3 -> third=   10000.0
-                    R.id.green_button3 -> third=    100000.0
-                    R.id.blue_button3 -> third=     1000000.0
-                    R.id.purple_button3 -> third=   10000000.0
-                    R.id.gray_button3 -> third=     100000000.0
-                    R.id.white_button3 -> third=    1000000000.0
+            if (isChecked) {
+                when (chekedId) {
+                    R.id.black_button3 -> mainViewModel.assignValue("black", 3)
+                    R.id.brown_button3 -> mainViewModel.assignValue("brown", 3)
+                    R.id.red_button3 -> mainViewModel.assignValue("red", 3)
+                    R.id.orange_button3 -> mainViewModel.assignValue("orange", 3)
+                    R.id.yellow_button3 -> mainViewModel.assignValue("yellow", 3)
+                    R.id.green_button3 -> mainViewModel.assignValue("green", 3)
+                    R.id.blue_button3 -> mainViewModel.assignValue("blue", 3)
+                    R.id.purple_button3 -> mainViewModel.assignValue("purple", 3)
+                    R.id.gray_button3 -> mainViewModel.assignValue("gray", 3)
+                    R.id.white_button3 -> mainViewModel.assignValue("white", 3)
                 }
             }
         }
 
         mainBinding.toggleButtonGroup4.addOnButtonCheckedListener { toggleButtonGroup4, chekedId, isChecked ->
-            if (isChecked){
-                when (chekedId){
-                    R.id.golden_button1 -> tolerance=    0.05
-                    R.id.silver_button1 -> tolerance=    0.1
+            if (isChecked) {
+                when (chekedId) {
+                    R.id.golden_button1 -> mainViewModel.assignValue("golden", 4)
+                    R.id.silver_button1 -> mainViewModel.assignValue("silver", 4)
                 }
             }
         }
         with(mainBinding) {
             calculateButton.setOnClickListener {
-                total = (first+second)*third
-                tolerance_calculation = total*tolerance
-                ohmTextView.text = getString(R.string.info,total.toString(),tolerance_calculation.toString())
+                mainViewModel.calculateTotal()
+                mainViewModel.calculateTotalTolerance()
+                ohmTextView.text =
+                    getString(R.string.info, total.toString(), tolerance_calculation.toString())
             }
         }
     }
--- app/src/main/java/com/colosoft/resistorcalculator/ui/main/MainViewModel.kt
+++ app/src/main/java/com/colosoft/resistorcalculator/ui/main/MainViewModel.kt
@@ -1,11 +1,87 @@
 package com.colosoft.resistorcalculator.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
-class MainViewModel : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_view_model)
+class MainViewModel : ViewModel() {
+
+    var multiplier = 1
+    private var mutableFirst = 0.0
+    private var mutableSecond = 0.0
+    private var mutableThird = 1.0
+    private var mutableTolerance = 0.05
+
+    private val mutableTotal: MutableLiveData<Double> = MutableLiveData()
+    val mutableTotalDone: LiveData<Double> = mutableTotal
+
+    private val mutableTotalTolerance: MutableLiveData<Double> = MutableLiveData()
+    val mutableTotalToleranceDone: LiveData<Double> = mutableTotalTolerance
+
+
+    fun initialize() {
+        mutableTotal.value = 0.0
+    }
+
+    fun assignValue(colorName: String, column: Int) {
+
+        if (column == 1) {
+            multiplier = 10
+            when (colorName) {
+                "black" -> mutableFirst = 0.0 * multiplier
+                "brown" -> mutableFirst = 1.0 * multiplier
+                "red" -> mutableFirst = 2.0 * multiplier
+                "orange" -> mutableFirst = 3.0 * multiplier
+                "yellow" -> mutableFirst = 4.0 * multiplier
+                "green" -> mutableFirst = 5.0 * multiplier
+                "blue" -> mutableFirst = 6.0 * multiplier
+                "purple" -> mutableFirst = 7.0 * multiplier
+                "gray" -> mutableFirst = 8.0 * multiplier
+                "white" -> mutableFirst = 9.0 * multiplier
+            }
+        } else if (column == 2) {
+            multiplier = 1
+            when (colorName) {
+                "black" -> mutableSecond = 0.0 * multiplier
+                "brown" -> mutableSecond = 1.0 * multiplier
+                "red" -> mutableSecond = 2.0 * multiplier
+                "orange" -> mutableSecond = 3.0 * multiplier
+                "yellow" -> mutableSecond = 4.0 * multiplier
+                "green" -> mutableSecond = 5.0 * multiplier
+                "blue" -> mutableSecond = 6.0 * multiplier
+                "purple" -> mutableSecond = 7.0 * multiplier
+                "gray" -> mutableSecond = 8.0 * multiplier
+                "white" -> mutableSecond = 9.0 * multiplier
+            }
+        } else if (column == 3) {
+            when (colorName) {
+                "black" -> mutableThird = 1.0
+                "brown" -> mutableThird = 10.0
+                "red" -> mutableThird = 100.0
+                "orange" -> mutableThird = 1000.0
+                "yellow" -> mutableThird = 10000.0
+                "green" -> mutableThird = 100000.0
+                "blue" -> mutableThird = 1000000.0
+                "purple" -> mutableThird = 10000000.0
+                "gray" -> mutableThird = 100000000.0
+                "white" -> mutableThird = 1000000000.0
+            }
+        } else if (column == 4) {
+            when (colorName) {
+                "golden" -> mutableTolerance = 0.05
+                "silver" -> mutableTolerance = 0.1
+            }
+        }
+
+    }
+
+    fun calculateTotal() {
+        mutableTotal.value = (mutableFirst + mutableSecond) * mutableThird
+    }
+
+    fun calculateTotalTolerance() {
+        mutableTotalTolerance.value =
+            ((mutableFirst + mutableSecond) * mutableThird) * mutableTolerance
     }
 }
+
